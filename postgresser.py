d1e87ee7171828a5c72cdcea62592c4041effda5
zaharkoragin <koryagin03@icloud.com>
2022-03-14 03:30:02 +0300

    posgraser fix

--- postgresser.py
+++ postgresser.py
@@ -8,13 +8,13 @@ class Postgresser:
 
     def add_user(self, user_id):
         with self.connection:
-            if not self.cursor.execute(f"SELECT user_id FROM users WHERE user_id = {user_id}"):
+            if not self.cursor.execute(f"SELECT user_id FROM users WHERE user_id = %s",(user_id)):
                 self.cursor.execute(f"INSERT INTO users (user_id, status) VALUES (%s, %s)", (user_id, True))
                 self.connection.commit()
 
     def remove_user(self, user_id):
         with self.connection:
-            self.cursor.execute(f"DELETE FROM users WHERE user_id = {user_id}")
+            self.cursor.execute(f"DELETE FROM users WHERE user_id = %s",(user_id))
             self.connection.commit()
 
 
