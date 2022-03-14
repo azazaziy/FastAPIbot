93f323d5a8598289c17c57bd703c3d611708b952
zaharkoragin <koryagin03@icloud.com>
2022-03-14 04:29:51 +0300

    posgraser fix 6.1

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from fastapi import FastAPI
 from fastapi import Query
 from fastapi import Request
 
-
-from qwerty import bot
-from qwerty import db
 from messanger import write_json
 from messanger import running
 import json
+
+from postgresser import Postgresser
+from telebot import Bot
+
+db = Postgresser('postgres://vazkibwucrvyqe:3992c06da77a8456a1916565b80839b38c43ce7924d84a9327d8caa0fb12f7e8@ec2-176-34-105-15.eu-west-1.compute.amazonaws.com:5432/d6lc9s12vi2h2o')
+bot = Bot(TOKEN='1139412331:AAHH5phrKI8YLOtKPYm9VcwIpZUL23PpWIg')
+
+
 
 app = FastAPI()
 @app.get('/')
