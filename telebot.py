f3b86527c3ce9e6f7f1ae77c6360b37cae920c44
zaharkoragin <koryagin03@icloud.com>
2022-03-14 04:12:35 +0300

    posgraser fix 6.1

--- telebot.py
+++ telebot.py
@@ -1,12 +1,11 @@
 import requests
-
+from messanger import db
 
 
 class Bot:
 
-    def __init__(self, TOKEN, DATABASE):
+    def __init__(self, TOKEN):
         self.url = f'https://api.telegram.org/bot{TOKEN}/'
-        self.database = DATABASE
         self.webhook = f'{self.url}setWebhook?url=https://telegramwebhookapi.herokuapp.com/bot'
 
     def get_updates(self):
@@ -20,13 +19,13 @@ class Bot:
         return response.json()
 
     def add_u(self, chat_id):
-        self.database.add_user(chat_id)
+        db.add_user(chat_id)
 
     def remove_u(self, chat_id):
-        self.database.remove_user(chat_id)
+        db.remove_user(chat_id)
 
     def return_users_id(self):
-        temp = self.database.return_users()
+        temp = db.return_users()
         return [x for t in temp for x in t]
 
 # curl -X "POST" "https://api.telegram.org/bot1139412331:AAHH5phrKI8YLOtKPYm9VcwIpZUL23PpWIg/setWebhook" -d '{"url": "https://36f10ccf8c4b91.lhrtunnel.link"}' -H 'Content-Type: application/json; charset=utf-8'
